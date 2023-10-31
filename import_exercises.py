de9c59c83918e9af2aad8c62abdbb7fa578706fa
Stanley Burke <stanley.burke08@gmail.com>
2023-10-31 16:19:07 -0500

    reviwed and corrected

--- import_exercises.py
+++ import_exercises.py
@@ -1,7 +1,7 @@
 import function_exercises as f
 import itertools as i
 import json
-json.load(open('profiles.json'))
+profile = json.load(open('profiles.json'))
 
 
 f.calculate_tip(.05,100)
@@ -10,12 +10,25 @@ f.normalize_name("#########Stanny big big")
 
 f.is_two(2)
 
-marker = list(map(".".join,list(i.combinations("abc123",2))))
-marker3 = list(map(".".join,list(i.combinations("abc123",3))))
-marker2 = list(map(".".join,list(i.permutations("abcd",2))))
+marker = list(i.product("abc",[1,2,3]))
+marker2 = list(i.combinations("abcd",2))
+marker3 = list(i.permutations("abcd",2))
 
 print(marker)
+print(len(marker))
 
 print(marker2)
+print(len(marker2))
 
 print(marker3)
+print(len(marker3))
+active_users = 0
+inactive_users = 0
+for l in profile:
+    if l["isActive"]==True:
+            active_users += int(l["isActive"]) 
+for e in profile:
+    if e["isActive"]==False:
+            inactive_users += int(e["isActive"]) 
+print(f"The number of active users is {active_users}, and the number of inactive users is {inactive_users}")
+
